9c2facaf70a06f477d9bd788b3c2d3e1afffa9b9
aahel <guha.aahel@yahoo.com>
2016-10-29 16:50:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
-
+var config={
+    user:aahel,
+    database:aahel,
+    host:db.imad.hasura-app.io,
+    port:5432,
+    password:process.env.DB_PASSWORD
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM USER',function(req,res){
+        if(err)
+        res.status(500).send(err.toString());
+        else
+        res.send(JSON.stringify(result));
+    });
 });
 app.get('/article-one',function (req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
