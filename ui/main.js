15a48a78c56e376578e894db0c1b836de78b840e
aahel <guha.aahel@yahoo.com>
2016-11-11 17:28:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,104 +1,146 @@
-cfunction changediv(user_name)
-{
-    var changedivtemp=`<h1> Hi `+user_name+` </h1><br><h1><a href="/logout"> Logout</a></h1>`;
-    document.getElementById('loginwindow').innerHTML=changedivtemp;
-    
-}
-function loadLogin () {
-    // Check if the user is already logged in
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                loadLoggedInUser(this.responseText);
-                changediv(this.responseText);
-            } else {
-                alert("User is not Logged in");
-            }
-        }
-    };
-    
-    request.open('GET', '/check-login', true);
-    request.send(null);
-}
-function loadLoggedInUser (username) {
-    var user_name=username;
-    
-    alert(`Welcome ! `+user_name+` logged in Succesfully`);
-}
-function loadLoginForm () {
-   
-    
-    
-    // Submit username/password to login
-    var submit = document.getElementById('loginbtn');
-    submit.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  submit.value = 'Sucess!';
-              } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
-              } else if (request.status === 500) {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              } else {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              }
-              loadLogin();
-          }  
-          // Not done yet
-        };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        
-        request.open('POST', '/login', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        submit.value = 'Logging in...';
-        
-    };
-    
-    var register = document.getElementById('registerbtn');
-    register.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  alert('User created successfully');
-                  register.value = 'Registered!';
-              } else {
-                  alert('Could not register the user');
-                  register.value = 'Register';
-              }
-          }
-        };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/create-user', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        register.value = 'Registering...';
-    
-    };
-}
 
 
-loadLoginForm();
-loadLogin();
+#leftfloat {
+    height: 100%;
+    width: 250px;
+    border-right: thick solid black;
+    padding-top: 20px;
+}
+
+#header {
+    height: 130px;
+    text-align: center;
+    border-bottom: thick solid black;
+}
+
+body {
+    margin-right: -4px;
+}
+
+#articleplace {
+    margin-left: 300px;
+    margin-top: -635px;
+    margin-right: 20px;
+    display: inline-block;
+    position: absolute;
+    height: 100%;
+    width: 550px;
+}
+
+.container {
+    background-color: darkgray;
+    background-image: url(/ui/alone.jpg);
+    background-size: cover;
+    height: 700px;
+    width: 100%;
+}
+
+#navbar {
+    margin-top: -8px;
+    margin-left: -6px;
+    margin-right: 0px;
+    height: 90px;
+    width: 100%;
+    padding-top: 0px;
+    text-align: left;
+    padding-top: 6px;
+}
+
+#menulist {
+    display: inline;
+    position: fixed;
+    list-style-type: none;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: 24;
+    text-align: center;
+}
+
+.nav {
+    display: inline;
+    margin-left: 30px;
+}
+
+.nav:hover {
+    color: deepskyblue;
+    text-decoration: underline;
+    background-color: whitesmoke;
+}
+
+#loginpage {
+    background-image: url("/ui/bc.jpeg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 700px;
+    width: 100%;
+}
+
+.anchor {
+    color: black;
+    text-decoration: none;
+}
+
+#loginwindow {
+    margin-left: 75px;
+    margin-top: 275px;
+    background-color: transparent;
+    height: 250px;
+    width: 500px;
+    border: solid thick grey;
+    position: absolute;
+    padding-top: 50px;
+    padding-left: 20px;
+    text-align: center;
+    font-size: 17;
+}
+
+.loginw {
+    position: absolute;
+    font-size: 16;
+    font-family: Cambria, Cochin, Georgia, Times, Times New Roman, serif;
+}
+
+#loginbtn {
+    background-color: darkslategray;
+    font-size: 15;
+    height: 35px;
+    width: 50px;
+    border-radius: 8px;
+}
+
+#registerbtn {
+    background-color: indianred;
+    font-size: 15;
+    height: 35px;
+    width: 100px;
+    border-radius: 8px;
+}
+
+#registerbtn:hover {
+    background-color: whitesmoke;
+    font-size: 30;
+    height: 50px;
+    width: 150px;
+}
+
+#loginbtn:hover {
+    background-color: whitesmoke;
+    font-size: 30;
+    height: 50px;
+    width: 150px;
+}
+
+#loginimg {
+    position: absolute;
+    height: 300px;
+    width: 300px;
+    margin-top: 270px;
+    margin-left: 800px;
+}
+
+#contactme {
+    background-image: url(/ui/new.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 700px;
+    width: 100%;
+}
