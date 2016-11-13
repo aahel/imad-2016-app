d8c9eb3eef041408c04f3abaa7c81e357cb38a07
aahel <guha.aahel@yahoo.com>
2016-11-13 16:17:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 /* activate sidebar */
-$('#sidebar').affix({
+/*$('#sidebar').affix({
   offset: {
     top: 10
   }
@@ -27,13 +27,13 @@ $('#sidebar').affix({
 
 
 /* activate scrollspy menu */
-$('body').scrollspy({
+/*$('body').scrollspy({
   target: '#navbar-collapsible',
   offset: 50
-});
+});*/
 
 /* smooth scrolling sections */
-$('a[href*=#]:not([href=#])').click(function() {
+/*$('a[href*=#]:not([href=#])').click(function() {
   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -55,7 +55,7 @@ $(".dropdown-menu li a").click(function(){
 });
 
 
-function applyAnimation(){
+/*function applyAnimation(){
     var imagePos = $(this).offset().top;
     
     var topOfWindow = $(window).scrollTop();
@@ -141,7 +141,7 @@ counterTxt.innerHTML= counter.toString();
 
 
 //TESTING WITH MYSELF of COUNTER:
-var btn=document.getElementById('ctrbtn');
+/*var btn=document.getElementById('ctrbtn');
 btn.onclick=function(){
 
 var request=new XMLHttpRequest();
@@ -182,4 +182,111 @@ var name=nameInput.value;
 request.open('GET','http://localhost:8080/submit_name?name='+name,true);
 request.send(null);
 
-};
+};*/
+function changediv(user_name)
+{
+    
+    var divhtml="<h2>Hi,"+ user_name+"</h2> <br><h1><a href='/logout'>Logout</a></h1><br><br><br><br><br><br><br><br>";
+    document.getElementById("login").innerHTML= divhtml;
+    
+}
+function loadLoginForm () {
+   
+    
+    // Submit username/password to login
+    var submit = document.getElementById('loginbtn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+       
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
+    
+    
+    var register = document.getElementById('registerbtn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
+
+function loadLoggedInUser (username) {
+    alert("Login Successful!\n\nWelcome "+username+"!");
+    changediv(username);
+}
+
+function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+                
+            } 
+            
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+
+
+// The first thing to do is to check if the user is logged in!
+loadLogin();
+
+// Now this is something that we could have directly done on the server-side using templating too!
+loadLoginForm();
